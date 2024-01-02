890d8f01815db0a05214b26284578a5bdff27eca
czapkiewiczpaulina <150005196+CzapkiewiczPaulina@users.noreply.github.com>
2024-01-02 20:29:10 +0100

    został dodany nowy plik

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-const fetchCountries = async name => {
-  try {
-    const response = await fetch(
-      `https://restcountries.com/v2/name/${name}?fields=name.official,capital,population,flags.svg,languages`
-    );
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error(`Error fetching data: ${error.message}`);
-  }
-};
-
-export default fetchCountries;
-
 import fetchCountries from './fetchCountries.js';
 
 const searchBox = document.getElementById('search-box');
